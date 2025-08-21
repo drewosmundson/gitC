92b451fb86422d66ce4d540bc3e04ef7083f4c0f
Drew Osmundson <drewosmundson@gmail.com>
2025-08-21 01:58:47 -0500

    dir creation and config HEAD

--- got.c
+++ got.c
@@ -19,15 +19,44 @@
 
 
 
+// there can be just one stash
+// is local to own machine
+int stash(char* name){return 0;}
 
-int createBranch(char* dirname, char* branchName) {
 
 
-  return 0;
+// pull from specified branch 
+// if there are new changes then 
+int pull(){ return 0;
+
 }
 
 
+// read from file where the current head is pointing
+// must be the most recent commit otherwise will warn user
+// then create a temp branch at the commit then prompt the user
+// to open the GUI if they want to merge with the most current
+// branch end
+int commit(char* message){
 
+
+
+}
+
+
+int addFilesToVCS() {
+  return 0;
+}
+
+ 
+int createConfigFile(){
+  FILE *fp;
+  fp = fopen("vcs/HEAD.conf", "w");
+  if(fp == NULL){
+    printf("Error Config file not initalized");
+  }
+  fprintf(fp, "currentBranch=main");
+}
 
 // creates an empty directory and checks for success;
 // returns 0 on success, 1 on directory already exists, -1 on fail
@@ -35,30 +64,42 @@ int createEmptyDirectory(char* dirname){
   mode_t permissions = S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP | S_IWGRP | S_IXGRP;
   errno = 0;
   int directorySuccess  = mkdir(dirname, permissions);
-  if(errno != EEXIST){
-    printf("directory already exists");
+  if(errno == EEXIST){
+    printf("directory already exists\n");
     return 1;
   }
   else if (directorySuccess != 0) {
-    printf("error creating directory");
+    printf("error creating directory\n");
     return 1;
   }
   else {
     return 0;
   }
 }
+// this funciton will be more important in the future but currently is a glorified wrapper for 
+// create empty directroy once ive implemented add and commit then i will work with branches
+int newBranch(char* dirname, char* branchName) {
+  int pathStringLength = sizeof(dirname) + sizeof(branchName) + sizeof("/");
+  char branchPath[pathStringLength];
+  printf("%s\n", branchPath);
+  if(createEmptyDirectory(branchPath) == 1) {
+    return 1;
+  }
+  return 0;
+}
 
 int init(){
-  char* dirname = "vcs";
+  char* dirname = "vcs"; 
   if(createEmptyDirectory(dirname) == 1) {
-    printf("failed vcs directory creation");
+    printf("failed vcs directory creation\n");
     return 1;
   }
-  char* branchName = "main";
-  if(createBranch(dirname, branchName) == 1){
-    printf("failed initalizing main branch dir");
+  char* mainBranch = "vcs/main0";
+  if(createEmptyDirectory(mainBranch) == 1){
+    printf("failed initalizing main branch dir\n");
     return 1;
   }
+  createConfigFile();
 }
 
 // string comparison, this function returns 0 if the same, 1 if not the same
@@ -92,7 +133,6 @@ int protocol(char* userCommand, char** commandArray) {
       init();
       break;
     case 1: // add
-      add();
       break;
     case 2: // commit
       break;
